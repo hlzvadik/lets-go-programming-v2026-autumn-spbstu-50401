1b25b7e6b807b8b14999f5f894f5b1484b181394
hlzvadik <golcovv716@gmail.com>
2026-09-15 15:38:27 +0300

    Solve of task

--- goltsov.vadim/task-1/cmd/service/main.go
+++ goltsov.vadim/task-1/cmd/service/main.go
@@ -3,5 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var (
+		a         int
+		b         int
+		operation string
+	)
+	_, err1 := fmt.Scanln(&a)
+	_, err2 := fmt.Scanln(&b)
+	_, err3 := fmt.Scanln(&operation)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+	switch operation {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
